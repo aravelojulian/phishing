2ec6d6f4ec60ce9f79f5b9a396f9dfd27f6573a6
Alejandro Ravelo Julian <alejulian9119@gmail.com>
2022-11-09 00:07:34 -0500

    scripts

--- myapp/static/scripts/desicionTree.py
+++ myapp/static/scripts/desicionTree.py
@@ -1,15 +1,10 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
-
+import pandas as pd
 from sklearn import tree
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-
 from sklearn.feature_selection import mutual_info_classif
-
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 # número de rasgos a evaluar
 num_of_features = 48
@@ -33,42 +28,30 @@ for c in int_cols:
 
 data.rename(columns={'CLASS_LABEL': 'phishing'}, inplace=True)
 
-data['phishing'].value_counts().plot(kind='bar')
-
 X = data.drop(['id', 'phishing'], axis=1)
-y = data['phishing']
-
-discrete_features = X.dtypes == int
-
-my_phishing = mutual_info_classif(X, y, discrete_features=discrete_features)
-my_phishing = pd.Series(my_phishing, name='My Phishing', index=X.columns)
-my_phishing = my_phishing.sort_values(ascending=False)
-
-arr = []
-
-top_n_features = my_phishing.sort_values(ascending=False).head(num_of_features).index.tolist()
-X = data[top_n_features]
 y = data['phishing']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True)
 
 lr = tree.DecisionTreeClassifier()
-lr.fit(X_train, y_train)
+lr = lr.fit(X_train, y_train)
 
-y_pred = lr.predict(X_test)
-
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-accuracy = accuracy_score(y_test, y_pred)
-
-print(classification_report(y_test, y_pred))
-print()
-
-tree.plot_tree(lr)
+plt.figure()
+tree.plot_tree(lr, filled=True)
+plt.title("Árboles de decisión entrenado con " + str(int(num_of_values * 0.8)) + " casos")
 plt.show()
 
-arr.append([num_of_features, precision, recall, f1, accuracy])
+y_prediction = lr.predict(X_test)
+
+precision = precision_score(y_test, y_prediction)
+recall = recall_score(y_test, y_prediction)
+f1 = f1_score(y_test, y_prediction)
+accuracy = accuracy_score(y_test, y_prediction)
+
+print(classification_report(y_test, y_prediction))
+print()
+
+arr = [[num_of_features, precision, recall, f1, accuracy]]
 
 df = pd.DataFrame(arr, columns=['num_of_features', 'precision', 'recall', 'f1_score', 'accuracy'])
 print(df)
--- myapp/static/scripts/supportVectorMachine.py
+++ myapp/static/scripts/supportVectorMachine.py
@@ -1,17 +1,10 @@
-import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 from sklearn import svm
+from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
 from sklearn.model_selection import train_test_split
-from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.inspection import DecisionBoundaryDisplay
-from sklearn.ensemble import HistGradientBoostingClassifier
 
-
-# total de datos a evaluar
-num_of_values = 5001
+num_of_values = 5005
 test_size = 20
 
 data = pd.read_csv("../phishing.csv")
@@ -29,52 +22,23 @@ int_cols = data.select_dtypes('int64').columns
 for c in int_cols:
     data[c] = data[c].astype('int32')
 
-X = data.drop(['id', 'phishing'], axis=1)
+SC = StandardScaler()
+
+X = SC.fit_transform(data.drop(['id', 'phishing'], axis=1))
 y = data['phishing']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=(test_size / 100), shuffle=True)
 
-vm = HistGradientBoostingClassifier().fit(X, y)
+clf = svm.LinearSVC(max_iter=20000)
+clf.fit(X_train, y_train)
 
-# print(vm.score(X, y))
-# print(vm.decision_function(X_test)[5])
-# print(vm.predict(X_test)[5])
-y_pred = vm.predict(X_test)
+y_pred = clf.predict(X_test)
 
 precision = precision_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 accuracy = accuracy_score(y_test, y_pred)
 
-decision_function = vm.decision_function(X)
-support_vector_indices = np.where(np.abs(decision_function) <= 1 + 1e-15)[0]
-support_vectors = X[X.columns[0]][support_vector_indices]
-
-plt.subplot(1, 2, 1)
-plt.scatter(X[X.columns[0]], X[X.columns[1]], c=y, s=30, cmap=plt.cm.Paired)
-ax = plt.gca()
-
-DecisionBoundaryDisplay.from_estimator(
-    vm,
-    X,
-    ax=ax,
-    grid_resolution=50,
-    plot_method="contour",
-    colors="k",
-    levels=[-1, 0, 1],
-    alpha=0.5,
-    linestyles=["--", "-", "--"],
-)
-
-plt.scatter(
-    support_vectors.iloc[0],
-    support_vectors.iloc[1],
-    s=100,
-    linewidth=1,
-    facecolors="none",
-    edgecolors="k",
-)
-
-plt.title('Aymee')
-plt.tight_layout()
-plt.show()
+arr = [[precision, recall, f1, accuracy]]
+df = pd.DataFrame(arr, columns=['precision', 'recall', 'f1_score', 'accuracy'])
+print(df)
